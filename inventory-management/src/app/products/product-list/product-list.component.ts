638781af4a25695a7b743ee509ca9c4cd22e8dab
chakradharbanda <chakradharbanda1998@gmail.com>
2024-07-13 21:14:52 +0530

    PrimeNG

--- inventory-management/src/app/products/product-list/product-list.component.ts
+++ inventory-management/src/app/products/product-list/product-list.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { InventoryService, Product } from '../../inventory.service';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule, TableModule, ButtonModule, RouterModule],
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private inventoryService: InventoryService) { }
+  constructor(private inventoryService: InventoryService) {}
 
   ngOnInit(): void {
-    this.inventoryService.getProducts().subscribe(products => {
+    this.inventoryService.getProducts().subscribe((products) => {
       this.products = products;
     });
   }
